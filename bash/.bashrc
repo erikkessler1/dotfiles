ebd4fc3d85f2a131fac5ec92a6ba3f7d0076fb51
Erik Kessler <ekessler@salsify.com>
2018-09-04 12:58:27 -0400

    Add fzf
    Fuzzy searching

--- bash/.bashrc
+++ bash/.bashrc
@@ -5,3 +5,5 @@ source ~/.bashrc.d/iterm.bash
 source ~/.bashrc.d/prompt.bash
 source ~/.bashrc.d/commands.bash
 
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -4,6 +4,7 @@ alias serve="python -m SimpleHTTPServer 8000"
 alias oports="echo 'User:      Command:   Port:'; echo '----------------------------' ; lsof -i 4 -P -n | grep -i 'listen' | awk '{print \$3, \$1, \$9}' | sed 's/ [a-z0-9\.\*]*:/ /' | sort -k 3 -n |xargs printf '%-10s %-10s %-10s\n' | uniq"
 alias myip="ifconfig wlan0 | grep 'inet ' | cut --delimiter=' ' -f12 | sed s/addr://"
 alias em="/usr/local/Cellar/emacs-plus/25.3/bin/emacsclient"
+alias preview="fzf --preview 'bat --color \"always\" {}'"
 
 alias pulse="kubectl --namespace pulse"
 alias btf="kubectl --namespace below-the-fold"
