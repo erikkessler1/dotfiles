aab526d11896ad173bdd4f633b8cd3ec7b39bf40
Erik Kessler <ekessler@salsify.com>
2018-11-17 13:28:02 -0500

    Add more k8s commands

--- bash/.bashrc.d/commands.bash
+++ bash/.bashrc.d/commands.bash
@@ -1,1 +1,3 @@
 k8c() { salsifyk8s run cmd -a "$1" -e prod rails console "$2"; }
+k8r() { salsifyk8s run cmd -a "$1" -e prod bundle exec rake "$2" "${@:3}"; }
+k8s() { salsifyk8s "$2" -a "$1" -e prod "${@:3}"; }
