a94845ef04d22dfb05ad0a986960a66a2fa9157c
Erik Kessler <ekessler@salsify.com>
2019-04-01 09:04:38 -0400

    Rework emacs configuration

--- emacs/.emacs.d/configuration.el
+++ emacs/.emacs.d/configuration.el
@@ -1,81 +1,45 @@
 
-(defun sensible-defaults/comment-or-uncomment-region-or-line ()
-  "Comments or uncomments the region or the current line if
-there's no active region."
-  (interactive)
-  (let (beg end)
-    (if (region-active-p)
-        (setq beg (region-beginning) end (region-end))
-      (setq beg (line-beginning-position) end (line-end-position)))
-    (comment-or-uncomment-region beg end)))
+;;; -*- lexical-binding: t -*-
 
-(setq gc-cons-threshold 50000000) ;; GC limit
-(setq sentence-end-double-space nil)
-(add-hook 'before-save-hook
-            (lambda ()
-              (when buffer-file-name
-                (let ((dir (file-name-directory buffer-file-name)))
-                  (when (and (not (file-exists-p dir))
-                             (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
-                    (make-directory dir t))))))
-(delete-selection-mode t) ;; delete selected text when type something else
-(setq require-final-newline t) ;; add newlines to files
-(setq confirm-kill-emacs 'y-or-n-p)
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-(setq-default dired-listing-switches "-alh")
-(fset 'yes-or-no-p 'y-or-n-p)
-(global-font-lock-mode t) ;; syntax highlight whenever possible
-(global-auto-revert-mode t) ;; refresh files when changed
-(show-paren-mode t)
-(setq show-paren-delay 0.0)
-(setq ns-pop-up-frames nil) ;; opening files from finder opens in new buffer
-(global-set-key (kbd "M-;") 'sensible-defaults/comment-or-uncomment-region-or-line)
-
-(menu-bar-mode -1)
-(when window-system
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (tooltip-mode -1))
-
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-  backup-by-copying t    ; Don't delink hardlinks
-  version-control t      ; Use version numbers on backups
-  delete-old-versions t  ; Automatically delete excess backups
-  kept-new-versions 20   ; how many of the newest versions to keep
-  kept-old-versions 5    ; and how many of the old
-  )
-
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-(setq exec-path (append exec-path '("/usr/local/bin")))
-
-(setq ispell-program-name "/usr/local/bin/aspell")
-(add-to-list 'load-path "~/.emacs.d/resources")
-
-(setq backup-directory-alist
-      `(("." . ,(concat user-emacs-directory "backups"))))
-
-(require 'mouse) ;; needed for iterm2 compatibility
-
-(xterm-mouse-mode t)
-(unless window-system
-  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
-  (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-
-(server-start)
+(setq gc-cons-threshold 50000000)
 
 (package-initialize)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/")
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (require 'package)
-(unless (package-installed-p 'use-package) (package-refresh-contents) (package-install 'use-package))
+(unless (package-installed-p 'use-package) 
+  (package-refresh-contents) 
+  (package-install 'use-package))
 (eval-when-compile (require 'use-package))
 
 (use-package diminish
   :ensure t)
+
+(use-package use-package-ensure-system-package
+  :ensure t)
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq confirm-kill-emacs 'y-or-n-p)
+
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+(add-to-list 'load-path (concat user-emacs-directory "resources"))
+
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+      backup-by-copying t
+      version-control t ; use version numbers on backups
+      delete-old-versions t 
+      kept-new-versions 20
+      kept-old-versions 5)
+
+(server-start)
+
+(global-unset-key (kbd "C-x C-b"))
 
 (menu-bar-mode -1)
 (when window-system
@@ -83,10 +47,20 @@ there's no active region."
   (scroll-bar-mode -1)
   (tooltip-mode -1))
 
+(global-font-lock-mode t)
+
+(show-paren-mode t)
+(setq show-paren-delay 0.0)
+
 (setq-default column-number-mode t)
+
 (global-hl-line-mode t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+
+(use-package diff-hl
+  :ensure t
+  :config 
+  (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
+  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
 
 (use-package zenburn-theme
   :ensure t
@@ -97,15 +71,68 @@ there's no active region."
 (add-to-list 'default-frame-alist
              '(font . "Source Code Pro-18"))
 
-(use-package diff-hl
+(setq sentence-end-double-space nil)
+
+(delete-selection-mode t)
+
+(setq require-final-newline t)
+
+(global-auto-revert-mode t)
+
+(setq ns-pop-up-frames nil)
+
+(add-hook 'before-save-hook
+          (lambda ()
+            (when buffer-file-name
+              (let ((dir (file-name-directory buffer-file-name)))
+                (when (and (not (file-exists-p dir))
+                           (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
+                  (make-directory dir t))))))
+
+(require 'mouse) ;; needed for iterm2 compatibility
+
+(xterm-mouse-mode t)
+(unless window-system
+  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
+(use-package avy
   :ensure t
-  :config 
-  (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
+  :bind (("s-." . avy-goto-word-or-subword-1)
+         ("s-," . avy-goto-char))
+  :custom (avy-background t "darken the background"))
 
-(setq frame-title-format '((:eval (projectile-project-name))))
+(use-package ivy
+  :after counsel
+  :diminish
+  :bind (("C-c Cr" . ivy-resume)
+         ("C-x B" . ivy-switch-buffer-other-window))
+  :config (ivy-mode t)
+  :custom ((ivy-count-format "(%d/%d) ")
+           (ivy-use-virtual-buffers t)))
 
-(defun toggle-maximize-buffer () "Maximize buffer"
+(use-package counsel
+  :ensure t
+  :diminish
+  :config (counsel-mode t))
+
+(use-package swiper
+  :after counsel
+  :bind (("C-s" . swiper)))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+(use-package ag 
+  :ensure t
+  :ensure-system-package ag)
+
+(setq-default dired-listing-switches "-alh")
+
+(defun ek-toggle-maximize-buffer ()
+  "Maximize buffer or return to previous configuration"
   (interactive)
   (if (= 1 (length (window-list)))
       (jump-to-register '_) 
@@ -113,71 +140,22 @@ there's no active region."
       (window-configuration-to-register '_)
       (delete-other-windows))))
 
-(global-set-key (kbd "<s-return>") 'toggle-maximize-buffer)
+(global-set-key (kbd "<s-return>") 'ek-toggle-maximize-buffer)
 
-(defun other-window-kill-buffer ()
-  "Kill the buffer in the other window"
+(defun ek-copy-file-name ()
+  "Copy the current filename to the clipboard"
   (interactive)
-  ;; Window selection is used because point goes to a different window
-  ;; if more than 2 windows are present
-  (let ((win-curr (selected-window))
-        (win-other (next-window)))
-    (select-window win-other)
-    (kill-this-buffer)
-    (select-window win-curr)))
-(global-set-key (kbd "C-x K") 'other-window-kill-buffer)
-
-(add-to-list 'load-path "~/.emacs.d/resources/swiper")
-(add-to-list 'load-path "~/.emacs.d/resources/counsel-projectile")
-
-(require 'counsel)
-(require 'counsel-projectile)
-
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-
-(define-key ivy-minibuffer-map (kbd "C-m") 'ivy-alt-done)
-(setq projectile-completion-system 'ivy)
-(counsel-projectile-mode t)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
 
 (use-package ace-window
   :ensure t
-  :config
-  (global-set-key (kbd "M-o") 'ace-window)
-)
-
-(use-package ag :ensure t)
-(use-package projectile
-  :ensure projectile
-  :config 
-  (projectile-global-mode t)
-  (setq projectile-enable-caching t)
-  :diminish projectile-mode)
-
-(defun projectile-use-magit-if-possible ()
-  "If the project being switched to is a git repository, invoke
-magit-status on the project root directory. Use dired otherwise."
-  (interactive)
-  (if (and (executable-find "git")
-           (eq (projectile-project-vcs) 'git))
-      (magit-status (projectile-project-root))
-    (dired (projectile-project-root))))
-
-(setq projectile-switch-project-action 'projectile-use-magit-if-possible)
-
-(use-package avy
-  :ensure t
-  :bind (("s-." . avy-goto-word-or-subword-1)
-         ("s-," . avy-goto-char))
-  :config
-  (setq avy-background t))
+  :bind ("M-o" . ace-window)
+  :custom (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 
 (setq org-src-fontify-natively t)
 (setq org-src-tab-acts-natively t)
@@ -185,65 +163,96 @@ magit-status on the project root directory. Use dired otherwise."
 (use-package magit
   :ensure t
   :bind ("C-x g" . magit-status)
+  :config (add-hook 'git-commit-mode-hook 'turn-on-flyspell))
 
-  :config
-  (add-hook 'git-commit-mode-hook 'turn-on-flyspell))
-
-(use-package smartparens-config
-    :ensure smartparens
-    :config
-    (progn
-      (smartparens-global-mode)
-      (show-smartparens-global-mode t)))
-
-(use-package company               
-  :ensure t
-  :defer t
-  :init (global-company-mode)
-  :config
-  (progn
-    ;; Use Company for completion
-    (bind-key [remap completion-at-point] #'company-complete company-mode-map)
-
-    (setq company-tooltip-align-annotations t
-          ;; Easy navigation to candidates with M-<n>
-          company-show-numbers t)
-    (setq company-dabbrev-downcase nil)
-    (setq company-idle-delay 0))
-  :diminish company-mode)
-
-(use-package vue-mode
-  :ensure t
-  )
-
-(use-package neotree
-  :ensure t
-  :bind (("<f2>" . neotree-toggle))
-  :defer
-  :config)
-
-(add-to-list 'load-path "~/.emacs.d/resources/yasnippet")
-(require 'yasnippet)
-(yas-global-mode 1)
-(define-key yas-minor-mode-map (kbd "C-c y") yas-maybe-expand)
+(defun ek-comment-or-uncomment-region-or-line ()
+  "Comments or uncomments the region or the current line if
+  there's no active region."
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+        (setq beg (region-beginning) end (region-end))
+      (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)))
+(global-set-key (kbd "M-;") 'ek-comment-or-uncomment-region-or-line)
 
 (use-package aggressive-indent
   :ensure t)
 
-(use-package whitespace
-  :commands (whitespace-mode))
+(use-package smartparens
+  :config 
+  (smartparens-global-mode)
+  (show-smartparens-global-mode t)
+  :bind (("C-]" . sp-select-next-thing-exchange)
+         ("C-c s r" . sp-rewrap-sexp)
+         ("C-M-u" . sp-up-sexp)
+         ("C-M-d" . sp-down-sexp)))
+
+(use-package smartparens-config 
+  :ensure smartparens)
+
+(use-package company        
+  :ensure t
+  :diminish company-mode
+  :init (global-company-mode)
+  :bind ([remap completion-at-point] . company-complete)
+  :custom ((company-tooltip-align-annotations t)
+           (company-show-numbers t "Easy candidate navigation with M-<n>")
+           (company-idle-delay 0 "Show right away")
+           (company-dabbrev-downcase nil "Don't downcase stuff")))
+
+(use-package projectile
+  :ensure projectile
+  :diminish projectile-mode
+  :custom ((projectile-enable-caching t)
+           (projectile-keymap-prefix (kbd "C-c p"))
+           (projectile-completion-system 'ivy))
+  :config 
+  (projectile-global-mode t))
+
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-mode t))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package ruby-mode
+  :ensure t
+  :custom ((ruby-insert-encoding-magic-comment nil))
+  :config
+  (add-hook 'ruby-mode-hook 'subword-mode))
 
 (require 'chruby)
 (chruby "2.5.1")
+(add-hook 'projectile-after-switch-project-hook 'chruby-use-corresponding)
 
 (use-package rubocop
   :ensure t
   :defer t
   :init (add-hook 'ruby-mode-hook 'rubocop-mode))
 
+(defun ek-add-bundle (command)
+  (let ((exe (car command))
+        (opts (cdr command)))
+    (if (string-match "rubocop\\'" exe)
+        (if (member "--config" opts)
+            (append '("bundle" "exec" "rubocop") opts)
+          (cons exe (append opts '("--config" "/Users/ekessler/.my_rubocop.yml"))))
+      command)))
+
+(setq flycheck-command-wrapper-function (lambda (command)
+                                          (let ((modified-command (ek-add-bundle command))
+                                                (inhibit-message t))
+                                            (progn
+                                              (message "Original Command: %s" command)
+                                              (message "Modified Command: %s" modified-command)
+                                              modified-command))))
+
 (use-package rspec-mode
   :ensure t
-  :defer t
   :init 
   (add-hook 'ruby-mode-hook 'rspec-mode)
   (add-hook 'projectile-rails-mode 'rspec-mode))
@@ -252,19 +261,31 @@ magit-status on the project root directory. Use dired otherwise."
   :ensure t
   :init (projectile-rails-global-mode))
 
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-
-(add-to-list 'load-path "~/.emacs.d/resources/emacs-elixir")
-(require 'elixir-mode)
-
-(use-package yaml-mode
-  :ensure t
-  )
-
 (use-package web-mode
   :ensure t
   :defer t)
 
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+
+(use-package flymd 
+  :ensure t)
+
+(defun my-flymd-browser-function (url)
+  (let ((process-environment (browse-url-process-environment)))
+    (apply 'start-process
+           (concat "firefox " url)
+           nil
+           "/usr/bin/open"
+           (list "-a" "firefox" url))))
+
+(setq flymd-browser-open-function 'my-flymd-browser-function)
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook 'aggressive-indent-mode)
+  (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
+  (add-hook 'clojure-mode-hook 'subword-mode))
