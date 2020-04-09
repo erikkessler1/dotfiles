adc645cc18d3bccb570e4675bb535551376112d4
Erik Kessler <ekessler@salsify.com>
2020-04-09 08:22:12 -0400

    Fix k8c typo

--- bash/.bashrc.d/commands.bash
+++ bash/.bashrc.d/commands.bash
@@ -1,4 +1,4 @@
-k8c() { kubectl run-cmd --namespace "$1" --context prodution bundle exec rails console "$2"; }
+k8c() { kubectl run-cmd --namespace "$1" --context production bundle exec rails console "$2"; }
 k8c-s() { kubectl run-cmd --namespace "$1" --context staging bundle exec rails console "$2"; }
 k8r() { kubectl run-cmd --namespace "$1" --context production bundle exec rake "$2" "${@:3}"; }
 k8s() { kubectl "$2" --namespace "$1" -context prodution "${@:3}"; }
