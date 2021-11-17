99e746f85a5952462608a460073d633172bb0fab
Erik Kessler <ekessler@salsify.com>
2021-11-17 08:42:16 -0500

    Add a Brewfile

---
+++ Brewfile
@@ -0,0 +1,12 @@
+# Taps
+tap "d12frosted/emacs-plus"
+
+# Packages
+brew "chruby"
+brew "emacs-plus@27" --with-no-titlebar --with-nobu417-big-sur-icon
+brew "iterm"
+brew "ruby-install"
+brew "stow"
+
+# Casks
+cask "iterm"
--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if ! [[ $(command -v brew) ]]; then
+    echo 'Installing Homebrew...'
+    /bin/bash -c \
+        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" \
+        || echo 'Error installing Homebrew'
+    brew update
+else
+    echo 'Homebrew already installed'
+fi
+
+brew bundle --file=Brewfile
+
 stow bash
 stow emacs
 stow vscode -t "/Users/ekessler/Library/Application Support/Code/User/" --adopt
