a15c4bf0f787b890f3d6d09a4d07c353933a28d4
Erik Kessler <ekessler@salsify.com>
2023-11-03 12:46:47 -0400

    Setup colima

--- bash/.bashrc.d/integrations.bash
+++ bash/.bashrc.d/integrations.bash
@@ -26,3 +26,6 @@ export PATH="$VOLTA_HOME/bin:$PATH"
 # puppeteer
 export PUPPETEER_EXECUTABLE_PATH=$(brew --prefix)/bin/chromium
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+
+# colima
+export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
