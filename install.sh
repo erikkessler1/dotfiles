d918a319692ddc2e405ac607a55f37e6d82da796
Erik Kessler <erik.kessler1@gmail.com>
2024-12-04 13:03:11 -0500

    Fix VS Code stow
    We can't assume the username in the path.

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ brew bundle --file=Brewfile
 
 stow bash
 stow emacs
-stow vscode -t "/Users/ekessler/Library/Application Support/Code/User/" --adopt
+stow vscode -t "$HOME/Library/Application Support/Code/User/" --adopt
 
 # Link .bash_profile -> .bashrc
 rm -f ~/.bash_profile
